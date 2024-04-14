ea79544165c9bbc5f10884189be1edb88f2e3674
Lakshmanan <lakshmanan8836@mountzion.ac.in>
2024-04-14 23:46:17 +0530

    UI modified

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Cart",
+      title: "AutoCart-The Future Of Shopping",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.transparent),
         useMaterial3: true,
